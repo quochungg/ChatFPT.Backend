3f206adbb8d7031b929241d5f8b1d99bce297b07
QuocHung <hungnguyen@maple.com.vn>
2025-03-28 13:13:12 +0700

    delete migration

--- ChatFPT.API/ChatFPT.Insfracstructure/Migrations/20250326165019_InitialCreate.Designer.cs
+++ ChatFPT.API/ChatFPT.Insfracstructure/Migrations/20250326165019_InitialCreate.Designer.cs
@@ -4,6 +4,7 @@ using ChatFPT.Insfracstructure.Base;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ChatFPT.Insfracstructure.Migrations
 {
     [DbContext(typeof(ChatBoxDBContext))]
-    partial class ChatBoxDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250326165019_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
