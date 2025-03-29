19909a3a8c148a62e87c67bc239bd231692f115c
QuocHung <hungnguyen@maple.com.vn>
2025-03-30 00:02:34 +0700

    fix ai

--- ChatFPT.API/ChatFPT.Service/Services/AIService.cs
+++ ChatFPT.API/ChatFPT.Service/Services/AIService.cs
@@ -37,8 +37,7 @@ public class AIService : IAIService
         _httpClient = new HttpClient();
         _httpClient.DefaultRequestHeaders.Add("Api-Key", _pineconeApiKey);
         _unitOfWork = unitOfWork;
-        _mapper = mapper;
-    }
+        _mapper = mapper;    }
 
     public async Task<List<float>> GetEmbeddingAsync(string text)
     {
